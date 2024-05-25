62ceac36f7cbc6f1cbb5f3c6efeab783cc1d9548
Татьяна <ltasya72@gmail.com>
2024-05-25 18:01:03 +0300

    Закончен fashion-news

---
+++ js/app.js
@@ -0,0 +1,81 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const resizableSwiper = (
+    breakpoint,
+    swiperClass,
+    swiperSettings,
+    callback
+  ) => {
+    let swiper;
+
+    breakpoint = window.matchMedia(breakpoint);
+
+    const enableSwiper = function (className, settings) {
+      swiper = new Swiper(className, settings);
+
+      if (callback) {
+        callback(swiper);
+      }
+    };
+
+    const checker = function () {
+      if (breakpoint.matches) {
+        return enableSwiper(swiperClass, swiperSettings);
+      } else {
+        if (swiper !== undefined) swiper.destroy(true, true);
+        return;
+      }
+    };
+
+    breakpoint.addEventListener("change", checker);
+    checker();
+  };
+
+  const someFunc = (instance) => {
+    if (instance) {
+      instance.on("slideChange", function (e) {
+        console.log("*** mySwiper.activeIndex", instance.activeIndex);
+      });
+    }
+  };
+
+  resizableSwiper(
+    "(width <= 1289px)",
+    ".slider-1",
+    {
+      loop: true,
+      // spaceBetween: 30,
+      // slidesPerView: 2,
+      // pagination: {
+      //   el: ".swiper-pag",
+      //   clickable: true,
+      // },
+      navigation: {
+        nextEl: ".swiper-next",
+        prevEl: ".swiper-prev",
+      },
+      breakpoints: {
+        580: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+        },
+        485: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+      },
+    },
+    someFunc
+  );
+
+  // resizableSwiper("(max-width: 1500px)", ".slider-2", {
+  //   loop: true,
+  //   spaceBetween: 10,
+  //   slidesPerView: 3,
+  //   freeMode: true,
+  //   breakpoints: {
+  //     1200: {
+  //       spaceBetween: 20,
+  //     },
+  //   },
+  // });
+});
