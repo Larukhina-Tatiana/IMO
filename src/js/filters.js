88d3cc617e6c2131238fbfb7bc385d9e9500a374
Татьяна <ltasya72@gmail.com>
2024-07-11 15:54:36 +0300

    работа над фильтром

--- src/js/filters.js
+++ src/js/filters.js
@@ -9,8 +9,8 @@ $(".filter-style").styler();
 $(".js-range-slider").ionRangeSlider({
   skin: "round",
   type: "double",
-  min: 1000,
-  max: 5000,
+  // min: 450,
+  // max: 5000,
 });
 
 // const rangeItems = document.querySelectorAll("[data-range]");
@@ -29,3 +29,47 @@ $(".js-range-slider").ionRangeSlider({
 //     });
 //   });
 // }
+
+const rangeSliderInit = () => {
+  // создаем функцию инициализации слайдера
+  const range = document.getElementById("range"); // Ищем слайдер
+  const inputMin = document.getElementById("min"); // Ищем input с меньшим значнием
+  const inputMax = document.getElementById("max"); // Ищем input с большим значнием
+
+  if (!range || !inputMin || !inputMax) return; // если этих элементов нет, прекращаем выполнение функции, чтобы не было ошибок
+
+  const inputs = [inputMin, inputMax]; // создаем массив из меньшего и большего значения
+
+  noUiSlider.create(range, {
+    // инициализируем слайдер
+    start: [2000, 5000], // устанавливаем начальные значения
+    tooltips: [true, true],
+    connect: true, // указываем что нужно показывать выбранный диапазон
+    range: {
+      // устанавливаем минимальное и максимальное значения
+      min: 0,
+      max: 5000,
+    },
+    step: 1, // шаг изменения значений
+  });
+
+  range.noUiSlider.on("update", function (values, handle) {
+    // при изменений положения элементов управления слайдера изменяем соответствующие значения
+    inputs[handle].value = parseInt(values[handle]);
+  });
+
+  inputMin.addEventListener("change", function () {
+    // при изменении меньшего значения в input - меняем положение соответствующего элемента управления
+    range.noUiSlider.set([this.value, null]);
+  });
+
+  inputMax.addEventListener("change", function () {
+    // при изменении большего значения в input - меняем положение соответствующего элемента управления
+    range.noUiSlider.set([null, this.value]);
+  });
+};
+
+const init = () => {
+  rangeSliderInit(); // запускаем функцию инициализации слайдера
+};
+window.addEventListener("DOMContentLoaded", init); // запускаем функцию init, когда документ будет загружен и готов к взаимодействию
