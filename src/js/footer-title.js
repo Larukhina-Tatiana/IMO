2059c79243e43b3c1b14840be2b1aecdadf5501e
Татьяна <ltasya72@gmail.com>
2024-07-13 12:04:35 +0300

    сделан accordeon

--- src/js/footer-title.js
+++ src/js/footer-title.js
@@ -6,9 +6,9 @@ if (document.querySelector(".footer")) {
   });
 }
 
-if (document.querySelector(".accordeon")) {
-  $(".accordeon__title").on("click", function () {
-    $(this).toggleClass("title--active");
-    $(this).next().slideToggle("200");
-  });
-}
+// if (document.querySelector(".accordeon")) {
+//   $(".accordeon__title").on("click", function () {
+//     $(this).toggleClass("title--active");
+//     $(this).next().slideToggle("200");
+//   });
+// }
