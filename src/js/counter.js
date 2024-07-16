51705dec7aacf49d25afe6daf92f9f3704fc13a9
Татьяна <ltasya72@gmail.com>
2024-07-16 21:41:23 +0300

    counter

---
+++ src/js/counter.js
@@ -0,0 +1,19 @@
+// ==================================
+// Counter
+// ==================================
+function increaseCount(e, el) {
+  var input = el.previousElementSibling;
+  var value = parseInt(input.value, 10);
+  value = isNaN(value) ? 0 : value;
+  value++;
+  input.value = value;
+}
+function decreaseCount(e, el) {
+  var input = el.nextElementSibling;
+  var value = parseInt(input.value, 10);
+  if (value > 1) {
+    value = isNaN(value) ? 0 : value;
+    value--;
+    input.value = value;
+  }
+}
