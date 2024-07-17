c6567a84a3af642970cd026f7cc91c44f3c8d417
Татьяна <ltasya72@gmail.com>
2024-07-17 21:50:24 +0300

    src/html/page-card-products.html создаем счётчик и конзину

--- src/js/quantity.js
+++ src/js/quantity.js
@@ -1,14 +1,24 @@
 window.addEventListener("click", function (event) {
-  console.log("Click window!");
-  console.log(event.target.dataset.action);
-  // Проверяем, является ли элемент по которому кликнули  - кнопкой плюс
-  // if (event.target.dataset.action === "plus") {
-  //   console.log("Plus");
-  //   const counterBlock = event.target.closest(".quantity");
-  //   console.log(counterBlock);
-  // }
-  // // Проверяем, является ли элемент по которому кликнули  - кнопкой минус
-  // if (event.target.dataset.action === "minus") {
-  //   const counterBlock = event.target.closest(".quantity");
-  // }
+  // проверяем клик строго по кнопкам плюс или минус
+  if (
+    event.target.dataset.action === "plus" ||
+    event.target.dataset.action === "minus"
+  ) {
+    // Проверяем, является ли элемент по которому кликнули  - кнопкой плюс
+    // находим родителя счётчика
+    const counterBlock = event.target.closest(".quantity");
+    // Находим див с числом счётчика
+    const counter = counterBlock.querySelector("[data-counter]");
+
+    if (event.target.dataset.action === "plus") {
+      counter.innerText = ++counter.innerText;
+    }
+    // // Проверяем, является ли элемент по которому кликнули  - кнопкой минус
+    if (event.target.dataset.action === "minus") {
+      // если больше 1 - уменьшаем
+      if (parseInt(counter.innerText) > 1) {
+        counter.innerText = --counter.innerText;
+      }
+    }
+  }
 });
