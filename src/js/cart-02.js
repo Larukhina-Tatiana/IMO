624d923a11c444bb0b86f6e8d9602430865001ef
Татьяна <ltasya72@gmail.com>
2024-07-18 21:44:26 +0300

    работа над удалением с корзины

--- src/js/cart-02.js
+++ src/js/cart-02.js
@@ -18,8 +18,22 @@ window.addEventListener("click", function (event) {
     };
     console.log(productInfo);
 
-    // Собранные данные подставляем в шаблон
-    const cartItemHTML = `
+    // Проверяем Если элемент уже в корзине
+    const itemInCart = cartWrapper.querySelector(
+      `[data-id="${productInfo.id}"]`
+    );
+    // console.log(itemInCart);
+
+    // Если товар в корзине
+    if (itemInCart) {
+      const counterElement = itemInCart.querySelector("[data-counter]");
+      counterElement.innerText =
+        parseInt(counterElement.innerText) + parseInt(productInfo.counter);
+    } else {
+      // Если товара нет в корзине
+
+      // Собранные данные подставляем в шаблон
+      const cartItemHTML = `
           <div class="product__info" data-id="${productInfo.id}">
         <div class=" product__img images__thumb images__thumb--cart">
         <img src=${productInfo.imgSrc} loading="lazy" decoding="async" alt="${productInfo.title}">
@@ -27,20 +41,27 @@ window.addEventListener("click", function (event) {
         <span class="product__color">${productInfo.color}</span>
         <span class="product__size">46</span>
         <span class="product__price">${productInfo.price}</span>
-         <div class="quantity">
-          <div class="quantity-button quantity-down" data-action="minus">-
-          </div>
-          
-          <div class="quantity-current" data-counter>${productInfo.counter}</div>
-          <div class="quantity-button quantity-up" data-action="plus">+
-          </div>
-          
+          <div class="quantity">
+            <div class="quantity-button quantity-down" data-action="minus">-</div>
+            <div class="quantity-current" data-counter>${productInfo.counter}</div>
+            <div class="quantity-button quantity-up" data-action="plus">+</div>
         </div>
         <span class="product__sum">2 468.99</span>
+        <button class="product__btn-del">Удалить товар</button>
       </div>`;
 
-    // Отображаем товар в корзине
+      // Отображаем товар в корзине
 
-    cartWrapper.insertAdjacentHTML("beforeend", cartItemHTML);
+      cartWrapper.insertAdjacentHTML("beforeend", cartItemHTML);
+    }
+
+    // Сбраываем счётчикв блоке карточки товара
+    card.querySelector("[data-counter]").innerText = "1";
   }
+
+  const delElement = document.querySelector(".product__btn-del");
+  $(delElement).on("click", function () {
+    console.log(event.target.closest(".product__info"));
+    // event.target.closest(".product__info").remove();
+  });
 });
