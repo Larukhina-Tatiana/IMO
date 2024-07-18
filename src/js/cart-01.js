fb77c87dad8a2d23a0606443e23f3df9176b24f1
Татьяна <ltasya72@gmail.com>
2024-07-18 13:57:27 +0300

    добавление товара в корзину

--- src/js/cart-01.js
+++ src/js/cart-01.js
@@ -1,15 +1,37 @@
+// Div внутри корзины в которой мы добавляем товар
+const cartWrapper = document.querySelector(".cart-wrapper");
+
 // проверяем клик на окне
 window.addEventListener("click", function (event) {
   // проверяем что клик был именно по кнопке "В корзину"
   if (event.target.hasAttribute("data-cart")) {
     // Находим карточку товара по кнопке которой кликнули
     const card = event.target.closest(".card");
-
+    // console.log(card);
     const productInfo = {
       id: card.dataset.id,
-      imgSrc: card.querySelector(".card__img").getAttribute("src"),
-
-      // console.log(card);
+      imgSrc: card.querySelector(".page-card__img").getAttribute("src"),
+      color: card.querySelector(".page-card_color").innerText,
+      title: card.querySelector(".page-card__title").innerText,
+      price: card.querySelector(".page-card__price-price").innerText,
+      counter: card.querySelector("[data-counter]").innerText,
     };
+    console.log(productInfo);
+
+    // Собранные данные подставляем в шаблон
+    const cartItemHTML = `
+          <div class="product__info">
+        <div class=" product__img images__thumb images__thumb--cart">
+        <img src=${productInfo.imgSrc} loading="lazy" decoding="async" alt="images">
+        </div>
+        <span class="product__color">${productInfo.color}</span>
+        <span class="product__size">46</span>
+        <span class="product__price">${productInfo.price}</span>
+        <span class="product__sum">2 468.99</span>
+      </div>`;
+
+    // Отображаем товар в корзине
+
+    cartWrapper.insertAdjacentHTML("beforeend", cartItemHTML);
   }
 });
