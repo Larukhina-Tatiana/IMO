f849ae2eb4e5a7d2f9c13c7c8d39695321fe3eb8
Татьяна <ltasya72@gmail.com>
2024-07-22 11:57:31 +0300

    modified:   src/js/chipbasket.js

--- src/js/chipbasket.js
+++ src/js/chipbasket.js
@@ -1,9 +1,11 @@
 // Находим кнопк "В корзину"
 const productBasket = document.querySelector(".card-product__basket");
+
 // Div внутри корзины в которой мы добавляем товар
-const chipbasketСontentДist = document.querySelector(
+const chipbasketСontentList = document.querySelector(
   ".chipbasket-content__list"
 );
+// console.log(chipbasketСontentList);
 const chipbasket = document.querySelector(".chipbasket");
 const chipbasketQuantity = document.querySelector(".chipbasket__quantity");
 const sum = document.querySelector(".sum");
@@ -24,6 +26,29 @@ const normalPrice = (str) => {
   return String(str).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, "$1 ");
 };
 
+const plusFullPrice = (currentPrice) => {
+  return (price += currentPrice);
+};
+
+const minusFullPrice = (currentPrice) => {
+  return (price -= currentPrice);
+};
+
+printFullPrice();
+const printQuantity = () => {
+  let productsListLength =
+    cartProductsList.querySelector(".simplebar-content").children.length;
+  cartQuantity.textContent = productsListLength;
+  productsListLength > 0
+    ? cart.classList.add("active")
+    : cart.classList.remove("active");
+};
+plusFullPrice(priceNumber);
+
+const printFullPrice = () => {
+  fullPrice.textContent = `${normalPrice(price)} ₽`;
+};
+
 const generateCartProduct = (
   id,
   img,
@@ -31,88 +56,104 @@ const generateCartProduct = (
   price,
   vendorcode,
   color,
+  size,
   fullsum
 ) => {
   return `
-				      <li class="chipbasket-content__item">
-                <article class="chipbasket-product" data-id="${id}">
-                  <div class="chipbasket-product__top">
-                    <img class="chipbasket-product__img" src="${img}" loading="lazy" decoding="async" alt="${title}">
-                    <div class="chipbasket-product__inner">
-                      <h3 class="chipbasket-product__title chipbasket-product__meaning">${title}</h3>
-                      <div class="chipbasket-product__vendorcode">Артикул:
-                        <span class="chipbasket-product__meaning">${vendorcode}</span>
-                      </div>
-                      <div class="chipbasket-product__color">Цвет:
-                        <span class="chipbasket-product__meaning">${color}</span>
-                      </div>
-                      <div class="chipbasket-product__size">Размер:
-                        <span class="chipbasket-product__meaning">46</span>
-                      </div>
+            <li class="chipbasket-content__item">
+              <article class="chipbasket-product" data-id="${id}">
+                <div class="chipbasket-product__top">
+                  <img class="chipbasket-product__img" src="${img}" loading="lazy" decoding="async" alt="${title}">
+                  <div class="chipbasket-product__inner">
+                    <h3 class="chipbasket-product__title chipbasket-product__meaning">${title}</h3>
+                    <div class="chipbasket-product__vendorcode">Артикул:
+                      <span class="chipbasket-product__meaning">${vendorcode}</span>
+                    </div>
+                    <div class="chipbasket-product__color">Цвет:
+                      <span class="chipbasket-product__meaning">${color}</span>
+                    </div>
+                    <div class="chipbasket-product__size">Размер:
+                      <span class="chipbasket-product__meaning">${size}</span>
                     </div>
                   </div>
-                  <div class="chipbasket-product__center">
-                    <div class="chipbasket-product__price">Цена:
-                      <span class="chipbasket-product__meaning chipbasket-product__meaning--block price">
-											${normalPrice(price)}
-											</span>
-                    </div>
-                    <div class="quantity">
-                      <div class="quantity-button quantity-down" data-action="minus">-
-                      </div>
-                      <div class="quantity-current" data-counter>1</div>
-                      <div class="quantity-button quantity-up" data-action="plus">+
-                      </div>
-                    </div>
-                    <div class="chipbasket-product__sum">Сумма:
-                      <span class="chipbasket-product__meaning chipbasket-product__meaning--block fullsum">
-											${normalPrice(fullsum)}  ₴
-											</span>
-                    </div>
-                    <button class="chipbasket-product__btn-del" aria-label="Удалить товар" data-action='del'></button>
-                  </div>
-                </article>
-                <div class="chipbasket-product__bottom">
-                  <div class="chipbasket__fullprice">
-                    Итого:
-                    <span class="chipbasket-product__fullprice chipbasket-product__meaning">2 468.99 ₴</span>
-                  </div>
-                  <a class="product__btn-basket button button--bg-main-color" href="">В корзину</a>
                 </div>
-              </li>
+                <div class="chipbasket-product__center">
+                  <div class="chipbasket-product__price">Цена:
+                    <span class="chipbasket-product__meaning chipbasket-product__meaning--block price">
+                    ${normalPrice(price)}
+                    </span>
+                  </div>
+                  <div class="quantity">
+                    <div class="quantity-button quantity-down" data-action="minus">-
+                    </div>
+                    <div class="quantity-current" data-counter>1</div>
+                    <div class="quantity-button quantity-up" data-action="plus">+
+                    </div>
+                  </div>
+                  <div class="chipbasket-product__sum">Сумма:
+                    <span class="chipbasket-product__meaning chipbasket-product__meaning--block fullsum">
+                    ${normalPrice(fullsum)}  ₴
+                    </span>
+                  </div>
+                  <button class="chipbasket-product__btn-del" aria-label="Удалить товар" data-action='del'></button>
+                </div>
+              </article>
+              <div class="chipbasket-product__bottom">
+                <div class="chipbasket__fullprice">
+                  Итого:
+                  <span class="chipbasket-product__fullprice chipbasket-product__meaning">2 468.99 ₴</span>
+                </div>
+                <a class="product__btn-basket button button--bg-main-color" href="">В корзину</a>
+              </div>
+            </li>
 	`;
 };
+// console.log(generateCartProduct);
 
-productBasket.forEach((el) => {
-  el.closest(".card-product").setAttribute("data-id", randomId());
-
-  el.addEventListener("click", (e) => {
-    let self = e.currentTarget;
-    let parent = self.closest(".card-product");
-    let id = parent.dataset.id;
-    let img = parent.querySelector(".card-product__image").getAttribute("src");
-    let title = parent.querySelector(".card-product__title").textContent;
+// productBasket.forEach((el) => {
+window.addEventListener("click", function (event) {
+  if (event.target.hasAttribute("data-cart")) {
+    // el.addEventListener("click", (e) => {
+    // console.log(productBasket.closest(".card-product"));
+    productBasket.closest(".card-product").setAttribute("data-id", randomId());
+    const card = event.target.closest(".card-product");
     let priceString = priceWithoutSpaces(
-      parent.querySelector(".card-product__price").textContent
+      card.querySelector(".card-product__price").textContent
     );
     let priceNumber = parseInt(
-      priceWithoutSpaces(
-        parent.querySelector(".card-product__price").textContent
-      )
+      priceWithoutSpaces(card.querySelector(".card-product__price").textContent)
     );
+    console.log(card);
+    const productInfo = {
+      id: card.dataset.id,
+      imgSrc: card.querySelector(".card-product__image").getAttribute("src"),
+      title: card.querySelector(".card-product__title").innerText,
+      color: card.querySelector(".card-product__color").innerText,
+      vendorcode: card.querySelector(".card-product__vendorcode").innerText,
+      size: card.querySelector(".card-product__size").innerText,
+      price: card.querySelector(".card-product__price").innerText,
+      // counter: card.querySelector("[data-counter]").innerText,
 
-    plusFullPrice(priceNumber);
-
-    printFullPrice();
-
-    cartProductsList
-      .querySelector(".simplebar-content")
-      .insertAdjacentHTML(
-        "afterbegin",
-        generateCartProduct(id, img, title, price, vendorcode, color, fullsum)
-      );
-    printQuantity();
-
+      // cartProductsList
+      //   .querySelector(".simplebar-content")
+      //   .insertAdjacentHTML(
+      //     "afterbegin",
+      //     generateCartProduct(
+      //       id,
+      //       img,
+      //       title,
+      //       price,
+      //       vendorcode,
+      //       color,
+      //       size,
+      //       fullsum
+      //     )
+      //   );
+      // printQuantity();
+    };
     self.disabled = true;
-  });
+    console.log(productInfo);
+    console.log(priceString);
+    console.log(priceNumber);
+  }
 });
